1760d829351521d709d61776e67dbf7da5db14cf
Lewis Yearsley <lewisyearsley@hollandandbarrett.com>
2020-09-17 10:09:19 +0100

    fix: ensure approov token is never overwritten

--- src/react-native-approov/support-files/ApproovFetchSupport.js
+++ src/react-native-approov/support-files/ApproovFetchSupport.js
@@ -1,15 +1,15 @@
 /**
  * Copyright 2020 CriticalBlue Ltd.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
  * including without limitation the rights to use, copy, modify, merge, publish, distribute,
  * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all copies or
  * substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
  * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
  * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
@@ -48,17 +48,17 @@ const approovFetch = async (url, args) => {
       }
     }
 
-    // authenticate the app with Approov servers and fetch an Approov token 
+    // authenticate the app with Approov servers and fetch an Approov token
     const result = await Approov.fetchApproovToken(url);
     console.log("Fetched Approov token: " + result.loggableToken);
 
     // delegate to the existing fetch API method, adding the Approov token as a new request header
     const fetchPromise = oldFetch(url, {
+      ...args,
       headers: {
         'Approov-Token' : '' + result.token,
-        ...args.headers,
+        ...(args.headers || {}),
       },
-      ...args,
     });
 
     // check for SSL pinning errors

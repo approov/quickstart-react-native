bd1bee78a4de7710115965755a6bf2018c8e7d9b
skiph <skiph@maplerise.com>
2020-12-21 12:47:31 -0800

    Improve reg-android plugin test

--- react-native.config.js
+++ react-native.config.js
@@ -6,23 +6,23 @@ module.exports = {
       options: [
         {
           name: '--expireAfter <duration>',
-          description: 'expire registration after duration (default: 1h)',
+          description: 'expire registration after duration',
           default: '1h',
         },
       ],
       func: (args, config, opts) => {
         console.log('reg-android: registers android debug APK for 1 hour.')
         console.log(`  args: ${args}`)
-        console.log(`  opts: ${opts}`)
+        console.log(`  opts: ${JSON.stringify(opts, null, 2)}`)
       },
       examples: [
         {
           desc: 'register android debug APK for 1 hour (default)',
-          cmd: 'reg-android',
+          cmd: 'react-native reg-android',
         },
         {
           desc: 'register android debug APK for 1 year, 2 days, 3 hours, and 4 minutes',
-          cmd: 'reg-android --expireAfter 1y2d3h4m',
+          cmd: 'react-native reg-android --expireAfter 1y2d3h4m',
         },
       ],
     },

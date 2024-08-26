db8e0f8de1ad46bcad9c203e958a13045e4e1d0a
ivo.liondov <ivol@Sanctuary.local>
2024-08-26 13:43:45 +0100

    Update native projects shapes_fetch

--- examples/shapes_fetch/android/app/src/main/java/com/shapes_axios/MainActivity.kt
+++ examples/shapes_fetch/android/app/src/main/java/com/shapes_axios/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.shapes_fetch
+package com.shapes_axios
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "shapes_fetch"
+  override fun getMainComponentName(): String = "shapes_axios"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- examples/shapes_fetch/ios/shapes_axios/AppDelegate.mm
+++ examples/shapes_fetch/ios/shapes_axios/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"shapes_fetch";
+  self.moduleName = @"shapes_axios";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
@@ -16,16 +16,7 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-  return [self getBundleURL];
-}
-
-- (NSURL *)getBundleURL
-{
-#if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
-#else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
+  return [self bundleURL];
 }
 
 - (NSURL *)bundleURL
@@ -36,4 +27,5 @@
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
+
 @end
